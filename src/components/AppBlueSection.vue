b9b51fc7115913daf4663b282aa7c897f0ae319d
SalvoBevilacqua <salvatorebevilacqua91@gmail.com>
2023-11-23 19:10:58 +0100

    add:blu_section

--- src/components/AppBlueSection.vue
+++ src/components/AppBlueSection.vue
@@ -1,14 +1,25 @@
 <script>
 export default {
-
+    data() {
+        return {
+            listInfo: [
+                { name: "DIGITAL COMICS", url: "./src/assets/img/buy-comics-digital-comics.png", },
+                { name: "DC MERCHANDISE", url: "./src/assets/img/buy-comics-merchandise.png", },
+                { name: "SUBSCRIPTION", url: "./src/assets/img/buy-comics-subscriptions.png", },
+                { name: "COMIC SHOP LOCATOR", url: "./src/assets/img/buy-comics-shop-locator.png", },
+                { name: "DC POWER VISA", url: "./src/assets/img/buy-dc-power-visa.svg", },
+            ]
+        }
+    }
 }
 </script>
 
 <template>
     <div class="container">
         <ul>
-            <li>
-
+            <li v-for="image in listInfo" :key="image.name">
+                <img :src="image.url" :alt="image.name">
+                <p>{{ image.name }}</p>
             </li>
         </ul>
     </div>
@@ -22,6 +33,19 @@ export default {
     height: 180px;
     padding: 0 2rem;
     color: white;
-    @include flex();
+
+    ul {
+        @include flex();
+
+        li {
+            height: 180px;
+            @include flex();
+            gap: .75rem;
+
+            img {
+                height: 70px;
+            }
+        }
+    }
 }
 </style>
